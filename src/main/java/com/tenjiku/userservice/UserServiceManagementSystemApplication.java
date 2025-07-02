912016def6eb2daced6e02bb4698eaaedabf90c9
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-07-02 23:30:04 +0530

    intial commit

---
+++ src/main/java/com/tenjiku/userservice/UserServiceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tenjiku.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceManagementSystemApplication.class, args);
+	}
+
+}
